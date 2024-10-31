7f954a54391cc86efc6c7a58dbec3d680d5ebd61
Nicolomecca <nico97.mecca@gmail.com>
2024-10-31 16:39:16 +0100

    up

---
+++ Progetto_SpringBoot_week_5/src/main/java/Nicolo_Mecca/Progetto_SpringBoot_week_5/ProgettoSpringBootWeek5Application.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_SpringBoot_week_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSpringBootWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSpringBootWeek5Application.class, args);
+	}
+
+}
---
+++ Progetto_SpringBoot_week_5/src/test/java/Nicolo_Mecca/Progetto_SpringBoot_week_5/ProgettoSpringBootWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_SpringBoot_week_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSpringBootWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
